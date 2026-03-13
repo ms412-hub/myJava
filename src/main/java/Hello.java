19ad27cf5435da6e8f349f618deb261452ab39b0
Ohminseok <oms50112@gmail.com>
2026-03-13 15:41:57 +0900

    소스 수정

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
     public static void main(String[] args){
         System.out.println("Hello!! My First Java Coding!!");
-        System.out.println("Yone 10/3/2");
+        System.out.println("You on");
     }
 }
