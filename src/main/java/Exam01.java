11c242435538bd623f505bb2dadf6072a4bf113b
Ohminseok <oms50112@gmail.com>
2026-03-23 15:51:33 +0900

    수정

---
+++ src/main/java/Exam01.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Exam01 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        int z = s.nextInt();
+        int y = s.nextInt();
+
+        System.out.println(z + "/" + y + "=" + (double) z/y);
+    }
+}
