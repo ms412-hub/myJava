5705dd0a0302193df05d5943828c23bb1ec96c19
Ohminseok <oms50112@gmail.com>
2026-03-25 15:06:38 +0900

    수정

--- src/main/java/Exam01.java
+++ src/main/java/Exam01.java
@@ -4,9 +4,14 @@ public class Exam01 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
 
-        int z = s.nextInt();
-        int y = s.nextInt();
+        int a;
+        int b;
 
-        System.out.println(z + " / " + y + " = " + (double) z/y);
+        a = s.nextInt();
+        b = s.nextInt();
+
+        double resDiv;
+        resDiv = (double) a/b
+        System.out.println(a + " / " + b + " = " + resDiv);
     }
 }
