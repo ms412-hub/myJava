afc884cc4da51eb871448bed8b64387008752e37
Ohminseok <oms50112@gmail.com>
2026-03-27 13:07:12 +0900

    수정

--- src/main/java/Exam01.java
+++ src/main/java/Exam01.java
@@ -4,13 +4,12 @@ public class Exam01 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
 
-        int a;
-        int b;
+        int a,b;
+        double resDiv;
 
         a = s.nextInt();
         b = s.nextInt();
 
-        double resDiv;
         resDiv = (double) a/b;
         System.out.println(a + " / " + b + " = " + resDiv);
 
