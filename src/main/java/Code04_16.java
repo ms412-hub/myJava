d678e47343d9ad4e973367bf3005f98456b0d73c
Ohminseok <oms50112@gmail.com>
2026-04-03 17:50:05 +0900

    수정

---
+++ src/main/java/Code04_16.java
@@ -0,0 +1,12 @@
+public class Code04_16 {
+    public static void main(String[] args) {
+        String str = "Java";
+
+
+        System.out.println(str.substring(0));
+        System.out.println(str.substring(1));
+        System.out.println(str.substring(2));
+        System.out.println(str.substring(3));
+        System.out.println(str.substring(4));
+    }
+}
