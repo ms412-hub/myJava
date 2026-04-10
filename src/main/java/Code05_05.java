4a5c0dc51cbab53a552da95127c2a409aabf8a4e
Ohminseok <oms50112@gmail.com>
2026-04-10 15:24:41 +0900

    수정

---
+++ src/main/java/Code05_05.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Code05_05 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        int num;
+
+        System.out.printf("숫자를 입력하십쇼.==> ");
+        num = s.nextInt();
+
+        if (num %2 == 0) {
+            System.out.println("짝수입니다.");
+        }
+        else {
+            System.out.println("홀수입니다.");
+
+        }
+    s.close();
+
+    }
+}
---
+++ src/main/java/Code05_07.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+
+public class Code05_07 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+    }
+}
