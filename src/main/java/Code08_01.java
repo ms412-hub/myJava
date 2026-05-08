f8f83467b65b4c20c85b6a0c9930efe43e4a09df
Ohminseok <oms50112@gmail.com>
2026-05-08 13:21:53 +0900

    수정

---
+++ src/main/java/Code08_01.java
@@ -0,0 +1,22 @@
+class Rabbit10{
+    String shape;
+    int xpos;
+    int ypos;
+    static int count;
+    //생성한 토끼의 수
+    Rabbit10() {
+        count++;
+    }
+}
+
+public class Code08_01 {
+    public static void main(String[] args){
+        System.out.println("객페 생성전의 총 토끼의 수==>"+Rabbit10.count);
+
+        Rabbit10 rabbit1 = new Rabbit10();
+        System.out.println("토끼 객체1 생성후의 총 토끼 수==>"+Rabbit10.count);
+
+        Rabbit10 rabbit2 = new Rabbit10();
+        System.out.println("토끼 객체2 생성 후의 총 토끼 수==>"+ Rabbit10.count);
+    }
+}
