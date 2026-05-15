14947678400751beaea1596bf18f7cd7ad6017f9
Ohminseok <oms50112@gmail.com>
2026-05-15 11:43:53 +0900

    수정

---
+++ src/main/java/Code09_03.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Code09_03 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int[] numAry = new int[4];
+        int hap = 0;
+
+        for (int i=0;i<=3;i++){
+            System.out.print("숫자 : ");
+            numAry[i] = s.nextInt();
+        }
+        for (int j=0;j<=3;j++) {
+            hap += numAry[j];
+        }
+        System.out.println("합계 ==> " + hap);
+
+        s.close();
+    }
+}
